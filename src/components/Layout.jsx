fcbdca3d19e0ddc447843511f4a11d3d2136ef96
Sara Garnero <volpina1986@gmail.com>
2025-09-10 12:14:15 +0200

    esercizio modificato

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,43 +1,66 @@
 import React from "react";
-import { colors, Container } from "@mui/material";
+import { Container } from "@mui/material";
 
 function Layout(props) {
   return (
-    <Container
-      maxWidth={false}
+    <div
       style={{
         backgroundColor: "#1468b6",
-        padding: 0,
         minHeight: "100vh",
+        padding: 0,
         margin: 0,
+        display: "flex",
+        flexDirection: "column",
       }}
     >
+      {/* Header */}
       <div
         style={{
           height: "100px",
           backgroundColor: "#d42993",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
         }}
       >
         <img
-          style={{
-            height: "100px",
-          }}
+          style={{ height: "100px" }}
           src="https://png.pngtree.com/element_pic/16/11/02/bd886d7ccc6f8dd8db17e841233c9656.jpg"
-        ></img>
+          alt="Header"
+        />
       </div>
-      {props.children}
+
+      {/* Contenuto principale (si espande per occupare lo spazio) */}
+      <div style={{ flex: 1, padding: "20px", boxSizing: "border-box" }}>
+        <Container
+          maxWidth={false}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "flex-start",
+            width: "100%",
+          }}
+        >
+          {props.children}
+        </Container>
+      </div>
+
+      {/* Footer normale (non fisso) */}
       <div
         style={{
           height: "100px",
           backgroundColor: "#d42993",
-          position:"fixed",
-          bottom:0,
-          right:0,
-          left:0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
         }}
       ></div>
-    </Container>
+    </div>
   );
 }
 
 export default Layout;
+
